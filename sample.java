168116848dfa178b8809e811da5dc4bdf208804e
root <root@ip-172-31-1-200.ap-south-1.compute.internal>
2024-02-12 12:43:55 +0000

    5th commit

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class FactorialExample{
  public static void main(String args[]){  
   int i,fact=2;  
   int number=6;//It is the number to calculate factorial    
-  for(i=2;i<=number;i++){    
+  for(i=3;i<=number;i++){    
       fact=fact*i;    
   }    
   System.out.println("Factorial of "+number+" is: "+fact);    
